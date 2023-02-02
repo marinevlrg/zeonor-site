3d18acee53dadcc19f5cc372fef22734426320ec
marinevlrg <marine.valorge@gmail.com>
2023-02-02 12:33:10 +0100

    ça sent la fin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 // src/Controller/IndexController.php
 namespace App\Controller;
 
-use App\Repository\RealisationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +9,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_index', methods: ['GET'])]
-    public function index(RealisationRepository $realisationRepository): Response
+    public function index(): Response
     {
-        return $this->render('index.html.twig', [
-            'realisation' => $realisationRepository->findAll(),
-        ]);
+        return $this->render('index.html.twig');
     }
 }
